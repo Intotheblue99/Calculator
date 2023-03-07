d8cac515df0e1cf5c0802a28d74d20da31017d21
trainboy <unanimoususer99@gmail.com>
2023-03-07 14:41:55 -0800

    add variables for product continuation

--- javascript.js
+++ javascript.js
@@ -21,30 +21,13 @@ let num2 =[];
 let compute = [num1,operator,num2];
 let isClicked = false;
 
-let jack1 = [];
-function pass1() {
-    jack1.push(2,3,4,5);
-    console.log(jack1)
-}
-pass1();
-function pass() {
-    if (jack1.includes(3)){
-        jack1.pop();
-        console.log(jack1);
-    };
-}
-pass();
 
-let cram = [1,2,3,4,5]
-//creat a num var the set its value to num1//
 function pressed(event) {
     let values = event.target.value;
     num += values;
     input.textContent = num;
-    // compute = [num1];
     if(isClicked===true){
         console.log('tell me');
-        // compute = num1+operator+num2;
         num2 += values;
         input.textContent = compute + num2;
     };
@@ -54,62 +37,38 @@ function pressed(event) {
 
 function equations(event) {
     num1 = num;
+    num2 = [];
     let operand = event.target.id;
     operator = operand;
     if (operator.includes(operand)){
         console.log('yes')
-        num1 = parseInt(num1);
         isClicked = true;
         compute = num1+operator;
         input.textContent = compute;
-        // input.textContent =num1+operator;
     };
 }
 
-// switch (compute) {
-//     case '+':
-//         console.log('addition');
-//         break
-    
-// }
-
-let a = [9];
-let b= [8];
-
-function smoke() {
-    let mass = parseInt(a) + parseInt(b);
-    console.log(mass)
-}
-smoke()
 
 const add = function(num1, num2) {
     console.log('hiroshima')
-    let pro = parseInt(num1) + parseInt(num2);
-    console.log(pro);
+    let sum = parseInt(num1) + parseInt(num2);
+    console.log(sum);
+    output.textContent = compute + num2 + '=';
+    num = sum;
+    input.textContent = sum;
+    num2=[];
 };
+
 
 function master() {
     console.log(num1,operator,num2);
     console.log(operator);
-    // compute = num1+operator+num2;
     console.log(compute)
-
+    //Make switch statements//
     if (compute.includes('+')) {
         add(num1,num2);
     } else {
         console.log('bruh...')
     }
   
-}
-
-// function second(event){
-//     let values = event.target.value;
-//     num2 += [values];
-//     input.textContent += num2;
-//     console.log('num2'+num2);
-// }
-
-// if (isClicked===true) {
-//     second();
-//     console.log('tell me');
-// };
+};
