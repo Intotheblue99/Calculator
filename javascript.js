7467e07ce372ddfdb7e7e85dd008692581f89228
trainboy <unanimoususer99@gmail.com>
2023-03-07 19:54:55 -0800

    fix subtraction bug

--- javascript.js
+++ javascript.js
@@ -15,6 +15,10 @@ let backspace = document.getElementById('#back');
 //         input.textContent = compute;
 //     });
 // })
+
+// const negative = document.getElementById('#-');
+// negative.addEventListener('click')
+
 let num = [];
 let num1 = [];
 let operator = [];
@@ -27,22 +31,23 @@ function pressed(event) {
     let values = event.target.value;
     num += values;
     input.textContent = num;
+    // if (num.includes('-')) {
+    //     num1 = num;
+    // };
+    
     if(isClicked===true){
-        console.log('tell me');
         num2 += values;
         input.textContent = compute + num2;
     };
-}
-
-
+};
 
 function equations(event) {
     num1 = num;
     num2 = [];
     let operand = event.target.id;
     operator = operand;
+    num = operand;
     if (operator.includes(operand)){
-        console.log('yes')
         isClicked = true;
         compute = num1+operator;
         input.textContent = compute;
@@ -50,14 +55,14 @@ function equations(event) {
 };
 
 const add = function(num1, num2) {
-    let sum = parseInt(num1) + parseInt(num2);
+    let sum = parseFloat(num1) + parseFloat(num2);
     output.textContent = compute + num2 + '=';
-    num = sum;
+    num = parseFloat(sum);
     input.textContent = sum;
     num2=[];
 };
 
-const subtract = function(num1, num2) {
+function subtract(num1, num2) {
     let difference = parseFloat(num1) - parseFloat(num2);
     output.textContent = compute + num2 + '=';
     num = parseFloat(difference);
@@ -66,7 +71,7 @@ const subtract = function(num1, num2) {
 };
 
 const multiply = function(num1, num2) {
-    let product = parseInt(num1) * parseInt(num2);
+    let product = parseFloat(num1) * parseFloat(num2);
     output.textContent = compute + num2 + '=';
     num = product;
     input.textContent = product;
@@ -75,16 +80,20 @@ const multiply = function(num1, num2) {
 
 
 
+
 function master() {
     //Make switch statements//
-    if (compute.includes('+')) {
+    if (operator==='+') {
         add(num1,num2);
     };
-    if(compute.includes('-')) {
+    if(operator==='-') {
         subtract(num1,num2)
     }; 
-    if (compute.includes('x')) {
+    if (operator==='x') {
         multiply(num1,num2)
+    };
+    if (operator==='÷') {
+        divide(num1,num2)
     };
   
 };
